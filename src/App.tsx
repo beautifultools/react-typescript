f7949a1429c4d7bdede7fc8025a95e93fdef2d4b
john <lord1229@gmail.com>
2021-07-04 22:35:46 +0900

    recoil 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { RecoilRoot } from "recoil";
 
 function App() {
   return (
